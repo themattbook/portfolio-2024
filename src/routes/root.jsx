194c4f602c1de732ff5ca4951ccd5e1fd5f6825f
Matthew Sweet <sweet@andrews.edu>
2024-03-09 12:51:24 -0600

    Projects data and UI

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -39,11 +39,14 @@ export default function Root() {
 					<div className='project-desc'>
 						<h3>Digital Freight Network</h3>
 						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing
-							elit. Eum assumenda tempora tempore perferendis
-							repudiandae eos minima, quae quaerat voluptatem
-							nostrum temporibus in et laborum optio fugiat nulla,
-							modi qui ducimus.
+							I was hired by DFN Freight to build a digital
+							logistics application that could handle all of the
+							legwork between a carrier and a shipper. I started
+							as the only dev, laying the groundwork that
+							propelled the app to MVP.
+						</p>
+						<p className='project-readmore'>
+							<a href=''>Read more &rarr;</a>
 						</p>
 					</div>
 				</div>
@@ -55,11 +58,13 @@ export default function Root() {
 					<div className='project-desc'>
 						<h3>Tornado Trax</h3>
 						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing
-							elit. Eum assumenda tempora tempore perferendis
-							repudiandae eos minima, quae quaerat voluptatem
-							nostrum temporibus in et laborum optio fugiat nulla,
-							modi qui ducimus.
+							As a hobby meterologist, Tornadoes are of a
+							particular interest of mine. I wanted to build an
+							interactive app that visualizes tornado paths using
+							NOAA weather data. This is an ongoing project.
+						</p>
+						<p className='project-readmore'>
+							<a href=''>Read more &rarr;</a>
 						</p>
 					</div>
 				</div>
@@ -69,13 +74,16 @@ export default function Root() {
 						className='project-image'
 					/>
 					<div className='project-desc'>
-						<h3>What Can I Make?</h3>
+						<h3>What&apos;s In The Fridge?</h3>
 						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing
-							elit. Eum assumenda tempora tempore perferendis
-							repudiandae eos minima, quae quaerat voluptatem
-							nostrum temporibus in et laborum optio fugiat nulla,
-							modi qui ducimus.
+							As a husband and father, I know all too well the
+							struggles of coming up with ideas for dinner. Since
+							recipe apps are a dime-a-dozen, I wanted an app that
+							takes the ingredients I already have on hand and
+							gives me recipe ideas.
+						</p>
+						<p className='project-readmore'>
+							<a href=''>Read more &rarr;</a>
 						</p>
 					</div>
 				</div>
@@ -85,13 +93,17 @@ export default function Root() {
 						className='project-image'
 					/>
 					<div className='project-desc'>
-						<h3>5 Day Forecast</h3>
+						<h3>Forecast and Alerts</h3>
 						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing
-							elit. Eum assumenda tempora tempore perferendis
-							repudiandae eos minima, quae quaerat voluptatem
-							nostrum temporibus in et laborum optio fugiat nulla,
-							modi qui ducimus.
+							Hear me out. I know I just talked about dime-a-dozen
+							apps, but this one was made to pick up where other
+							apps I&apos;ve seen fall short. When I check the
+							weather, I also want to be able to see active
+							weather alerts. I also don&apos;t want a lot of
+							distractions. Just give me the data. This is it.
+						</p>
+						<p className='project-readmore'>
+							<a href=''>Read more &rarr;</a>
 						</p>
 					</div>
 				</div>
